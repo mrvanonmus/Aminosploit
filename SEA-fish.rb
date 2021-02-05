5cf8e6dde5525d81cbb0bc30858dddcc345cd05f
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2021-02-05 16:31:35 +0200

    Add files via upload

---
+++ SEA-fish.rb
@@ -0,0 +1,70 @@
+system("clear")
+puts("       _____   _____       ___ \n.     /  ___/ | ____|     /   | \n      | |___  | |__      / /| | \n.     \|___ \ | |  __|    / /_| | \n       ___| | | |___   / /__| |\n      /_____/ |_____| /_/   |_|") 
+  puts("[SEA]==========================[SEA]") 
+  puts("       Dev : EBH with Volcano963$C2") 
+  puts("           the bro   hakem$C2")  
+  puts("           the bro   virus$C2")  
+  puts("       amino sploit in SEA $C2") 
+    
+  puts("[SEA]==========================[SEA]") 
+puts("\n            {1}Wishsix")
+puts("            {2}Blank page")
+puts("            {3}Allow follow-up requests")
+puts("            {4}amino")
+puts("\n       enter the number")
+num=gets.chomp.to_i
+ if num ==1
+  system("pkg install unzip")
+  
+  system("unzip Wishsix.zib")
+  
+  system("rm -rf Wishsix.zip")
+  
+  system("cd Wishsix")
+  
+  system("bash 1.sh")
+ 
+ elsif num ==2
+  system("pkg install unzip")
+  
+  system("unzip Blank-page.zib")
+  
+  system("rm -rf Blank-page.zip")
+  
+  system("cd Blank-page")
+  
+  system("bash 1.sh")
+  
+ elsif num ==3
+   system("pkg install unzip")
+   
+   system("unzip Allow.zib")
+  
+   system("rm -rf Allow.zip")
+  
+   system("cd Allow")
+  
+   system("bash 1.sh")
+   
+  elsif num ==4
+   system("pkg install unzip")
+  
+   system("unzip amino.zib")
+  
+   system("rm -rf amino.zip")
+  
+   system("cd amino")
+  
+   system("bash 1.sh")
+   
+   elsif num ==cd
+  
+   system("bash aminosploit.sh")
+   
+   elsif num ==exit
+   
+   system("exit")
+ else
+   system("ruby SEA-fish.rb")
+ end
+ puts "good by"
