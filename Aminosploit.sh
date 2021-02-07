77457e617ceb0e8ca519da345ab912c974f98dae
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2021-02-07 20:31:48 +0200

    Add files via upload

---
+++ Aminosploit.sh
@@ -0,0 +1,62 @@
+clear
+echo -e "\033[31m    ___       ___  ___   _   __   _   _____\e[0m   ";
+echo -e "\033[31m   /  | |    / |     /   | | | |  \ | | /  _  \\e[0m      ";
+echo -e "\033[31m  / /_| |   / /|    /| | | | |   \| | | | | |\e[0m   ";
+echo -e "\033[31m / /  | |  / /       | | | | | | \  | | |_| |\e[0m     ";
+echo -e "\033[31m/_/   |_| /_/        |_| |_| |_|  \_| \_____/\e[0m ";
+echo -e "  _____   _____   _       _____   _   _____ ";
+echo -e " /  ___/ |  _  \ | |     /  _  \ | | |_   _|   ";
+echo -e " | |___  | |_| | | |     | | | | | |   | |   ";
+echo -e " \___  \ |  ___/ | |     | | | | | |   | | ";
+echo -e "  ___| | | |     | |___  | |_| | | |   | |      ";
+echo -e " /_____/ |_|     |_____| \_____/ |_|   |_|    v1.0";
+
+printf "                                        \e[1;77m VOLCANO963 \e[0m \n"
+printf "\e[1;77m v2.0 SEA \e[0m \n"
+printf "\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Chat closed\e[0m"  ; printf "                   \e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m delete chat\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m delete blog\e[0m"  ; printf "                   \e[1;92m[\e[0m\e[1;77m04\e[0m\e[1;92m]\e[0m\e[1;93m send coins\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m05\e[0m\e[1;92m]\e[0m\e[1;93m apk amino tool and Virus\e[0m"  ; printf "      \e[1;92m[\e[0m\e[1;77m06\e[0m\e[1;92m]\e[0m\e[1;93m wish SEA\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m07\e[0m\e[1;92m]\e[0m\e[1;93m fishing sid\e[0m"  ; printf "                   \e[1;92m[\e[0m\e[1;77m08\e[0m\e[1;92m]\e[0m\e[1;93m payload.py\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m09\e[0m\e[1;92m]\e[0m\e[1;93m install kali linux\e[0m" ;printf "            \e[1;92m[\e[0m\e[1;77m10\e[0m\e[1;92m]\e[0m\e[1;93m Tool requirements\e[0m" 
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77mSEA\e[0m\e[1;92m] Enter the number : \e[0m' name 
+if [ "$name" = "1" ] ; then 
+     python Chat.py 
+
+elif [ "$name" = "2" ] ; then 
+    python dchat.py
+
+elif [ "$name" = "3" ] ; then 
+     python dblog.py
+
+elif [ "$name" = "4" ] ; then 
+     python coins.py
+
+elif [ "$name" = "5" ] ; then 
+     clear
+     pkg install ruby 
+     clear 
+     ruby apk-Virus.rb
+
+elif [ "$name" = "6" ] ; then 
+     bash wish-sex.sh 
+
+elif [ "$name" = "7" ] ; then 
+     pkg install unzip
+     unzip sites.zip
+     rm -rf sites.zip
+     bash ssid.sh
+
+elif [ "$name" = "8" ] ; then 
+      chmod +x *
+      python2 rat.py
+     
+elif [ "$name" = "9" ] ; then 
+     ruby kalilinux.rb
+     
+     elif [ "$name" = "10" ] ; then 
+     ruby sploit.rb
+else 
+     bash Aminosploit.sh
+fi
